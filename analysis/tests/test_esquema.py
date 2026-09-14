e7d0b7282bcb4d89c3f6b4c64289333ad2a464ed
Néstor O. Vásquez C. <nvasquezc@ucentral.edu.co>
2026-09-13 22:23:40 -0500

    ci: disparar primera corrida de workflows

--- analysis/tests/test_esquema.py
+++ analysis/tests/test_esquema.py
@@ -7,3 +7,4 @@ def test_esquema_soportado_no_vacio():
 
 def test_esquema_actual_soportado():
     assert 1 in ESQUEMA_SOPORTADO
+
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -494,3 +494,4 @@ void loop() {
   reintentarSensores();
   vigilarWiFi();
 }
+
